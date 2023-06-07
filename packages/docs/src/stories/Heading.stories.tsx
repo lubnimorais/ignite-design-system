42e8310122ca8e9996182b1f0cd3673a65f6e0b0
Lubni Morais <lubni.morais@gmail.com>
2023-06-06 22:24:21 -0300

    feat: Created component Heading

---
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,31 @@
+import type { StoryObj, Meta } from '@storybook/react';
+
+import { Heading, IHeadingProps } from '@ignite-ui/react';
+
+export default {
+  title: 'Typography/Heading',
+  component: Heading,
+  tags: ['autodocs'],
+
+  // são as propriedades
+  args: {
+    children: 'Custom title',
+  },
+} as Meta<IHeadingProps>;
+
+export const Primary: StoryObj<IHeadingProps> = {};
+
+export const CustomTag: StoryObj<IHeadingProps> = {
+  args: {
+    children: 'H1 Heading',
+    as: 'h1',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'Por padrão o heading sempre será um `h2`, mas podemos alterar isso com a propriedade `as`',
+      },
+    },
+  },
+};
